f0da4614d57297df4f3c5c295e49bbe265db660d
IsraelNascimentoo <israelbaiadonascimento@gmail.com>
2024-08-12 17:43:22 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,113 @@
+
+// Montador de HTML
+const loadProducts = (produtos, idDivParent) => {
+    const parentDiv = document.querySelector(idDivParent);
+    produtos.forEach((produto) => {
+        
+        const html = `
+            <article class="prato">
+                <img src="${produto.image}" alt="${produto.title}">
+                <h4>${produto.title}</h4>
+                <h4>R$${produto.value},00</h4>
+                <p>${produto.description}</p>
+                <button type="button" onclick="modalTrigger(${produto.id})">Quero este prato</button>
+            </article>
+            `
+
+        parentDiv.insertAdjacentHTML("beforeend", html)
+    });
+};
+
+
+
+//MODAL
+const modalTrigger = (productId) => {
+    const modal = document.querySelector(".modal");
+
+    if (productId != null) {
+        const produto = produtos.filter(produto => produto.id == productId)[0];
+        if ( produto != null) {
+            modal.querySelector("#title").value = produto.title;
+        }
+    }
+
+    if (modal.classList.contains("hide")) {
+        modal.classList.remove("hide");
+    } else {
+        modal.classList.add("hide");
+    }
+};
+
+
+const whatsappLinkGeneration = (phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment) => `https://api.whatsapp.com/send?phone=${phoneNumber}=&text=Olá, Eu quero: ${productQuantity} ${productTitle} - Entregar para, ${buyerName} no endereço: ${buyerAddress} - a forma de pagamento será: ${buyerPayment}`
+
+
+//SEND MESSAGE WHATSAPP
+const checkout = (phoneNumber) => {
+    const form = document.querySelector("#form-product");
+    form.addEventListener("submit", e => {
+        e.preventDefault()
+
+        const productTitle = form.querySelector("input#title").value
+        const productQuantity = form.querySelector("input#quantity").value
+        const buyerName = form.querySelector("input#name").value
+        const buyerAddress = form.querySelector("input#address").value
+        const buyerPayment = form.querySelector("select#payment").value
+     
+        const whatsappUrl = whatsappLinkGeneration(phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment);
+    
+        window.location.href = whatsappUrl
+    });
+}
+
+
+// BUSCADOR
+const search = (products, searchTerm) => products.filter( product => product.title.toLowerCase().includes(`${searchTerm.toLowerCase()}`) || 
+product.description.includes(`${searchTerm.toLowerCase()}`));
+
+
+const loadSearch = (form, productsDivId) => {
+    const producstDiv = document.querySelector(productsDivId);
+    const inputSearch = form.querySelector("#inputSearch")
+
+    //removendo pratos
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (inputSearch.value != "") {
+            producstDiv.querySelectorAll(".prato").forEach( (prato) => {
+                prato.remove()
+            })
+
+            const results = search(produtos, inputSearch.value)
+            // console.log(results);
+
+            results.forEach((produto) => {
+        
+                const html = `
+                    <article class="prato">
+                        <img src="${produto.image}" alt="${produto.title}">
+                        <h4>${produto.title}</h4>
+                        <h4>R$${produto.value},00</h4>
+                        <p>${produto.description}</p>
+                        <button type="button" onclick="modalTrigger(${produto.id})">Quero este prato</button>
+                    </article>
+                `
+        
+                producstDiv.insertAdjacentHTML("beforeend", html)
+            });
+        }
+
+    })
+}
+
+
+
+loadProducts(produtos, "#product-div")
+
+// checkout("5575998927538")
+
+checkout("5575998927538")
+loadSearch(document.querySelector("#formSearch"), "#product-div");
+
+
+// https://picsum.photos/200
---
+++ js/produtos.js
@@ -0,0 +1,52 @@
+const produtos = [
+    {
+        id: 1,
+        image: "src/img/soupa 1.jpg",
+        title: "Sopa",
+        description: "Venha Saborear uma deliciosa sopa.",
+        value: 10.00,
+    },
+    {
+        id: 2,
+        image: "src/img/Feijoada 1.jpg",
+        title: "Feijoada",
+        description: "Uma feijoada bem temperada para o seu almoço de domingo.",
+        value: 20.00,
+    },  
+    {
+        id: 3,
+        image: "src/img/prato3.jpg",
+        title: "Hambúrguer",
+        description: "Melhor Hambúrguer da Região.",
+        value: 12.00,
+    },
+    {
+        id: 4,
+        image: "src/img/gourmet 3.png",
+        title: "Gourmet de Morango",
+        description: "Picolé Gourmet de Morango, o queridinho da galera.",
+        value: 3.00,
+    },
+    {
+        id: 5,
+        image: "src/img/prato2.jpg",
+        title: "Sanduiche completo",
+        description: "Sanduiche completo com batata frita.",
+        value: 15.00,
+    },
+    {
+        id: 6,
+        image: "src/img/gourmet 1.jpg",
+        title: "Gourmet de Óreo",
+        description: "Picolé Gourmet de Óreo, sabor inesquecícel.",
+        value: 3.00,
+    },  
+    {
+        id: 7,
+        image: "src/img/gourmet 2.jpg",
+        title: "Gourmet de Lácta",
+        description: "Picolé Gourmet de Lácta, experiência única.",
+        value: 3.00,
+    },
+
+];
